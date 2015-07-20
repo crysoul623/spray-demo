9f700bae0f472687ae402fd7b20681ba08833fdc
zoro.lin <zoro.lin@gmail.com>
2015-07-21 00:21:38 +0800

    1.add spray client demo.

---
+++ spray/src/main/scala/shanpay/RequestLevelApiDemo.scala
@@ -0,0 +1,36 @@
+/**
+ *
+ */
+package shanpay
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.Failure
+import scala.util.Success
+
+import akka.actor.ActorSystem
+import akka.io.IO
+import akka.pattern.ask
+import akka.util.Timeout
+import spray.can.Http
+import spray.http._
+import spray.http.HttpMethods._
+
+/**
+ * @author Zoro
+ * request level api demo
+ */
+object RequestLevelApiDemo extends App {
+	
+    private implicit val timeout : Timeout = 5.seconds
+    
+    implicit val system = ActorSystem()
+    import system.dispatcher
+    
+    val response: Future[HttpResponse] = (IO(Http) ? HttpRequest(GET, Uri("http://spray.io"))).mapTo[HttpResponse]
+    
+    response onComplete {
+        case Success(httpResponse) => println(httpResponse.entity.asString)
+        case Failure(exception) => println(exception)
+    }
+}
